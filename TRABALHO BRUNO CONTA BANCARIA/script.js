f50b5020433fb448d70e1083209c7d2216a0fb0b
Cauê Vilcek Nogueira <126211634+CaueVilcek@users.noreply.github.com>
2023-05-09 21:55:14 -0300

    Add files via upload

---
+++ TRABALHO BRUNO CONTA BANCARIA/script.js
@@ -0,0 +1,27 @@
+let TudoArray = []
+let opcao = ''
+
+
+while (opcao != '4') {
+    opcao = prompt(`Sistema Pessoal de Financia\n
+    1 - Cadastrar Credito
+    2 - Cadastrar Debito
+    3 - Extrato dos Debitos e Creditos
+    4 - Sair`)
+
+    switch(opcao){
+        case '1':
+            let TudoArray = {}
+            imovel.proprietario = prompt('Digite o valor desejado')
+            imovel.quartos = prompt('Digite a quantidade de quartos')
+            imovel.banheiro = prompt('Digite a quantidade de banheiros')
+            imovel.garagem = prompt('Tem garagem ? S/N ?')
+        break;
+        case '2':
+            
+        break;
+        case '3':
+            
+        break;
+    }
+}
